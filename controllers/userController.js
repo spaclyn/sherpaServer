27557956b47630f577f4283b4ca4db3f1ff52dc5
Nellie Crocker <crocker.nellie@gmail.com>
2021-07-10 12:56:33 -0400

    finished user controller

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,16 +1,58 @@
 const router = require("express").Router()
 const { UserModel } = require('../models')
+const { UniqueConstraintError } = require("sequelize/lib/errors")
 
-router.post("/register", async (req,res) => {
+router.post("/register", async (req, res) => {
     let { email, password, name, location } = req.body.user
-    await UserModel.create({
-        email,
-        password,
-        name,
-        location
-    })
-    res.send("This is our register endpoint!")
+    try {
+        const User = await UserModel.create({
+            email,
+            password,
+            name,
+            location
+        })
+        res.status(201).json({
+            message: "User successfully registered",
+            user: User
+        })
+    } catch (err) {
+        if (err instanceof UniqueConstraintError) {
+            res.status(409).json({
+                message: "Email already in use",
+            })
+        } else {
+            res.status(500).json({
+                message: "Failed to register user",
+            })
+        }
+    }
 })
 
+router.post("/login", async (req, res) => {
+    let { email, password } = req.body.user
+
+    try {
+        const loginUser = await UserModel.findOne({
+            where: {
+                email: email,
+            }
+        })
+
+        if (loginUser) {
+            res.status(200).json({
+                user: loginUser,
+                message: "User successfully logged in!"
+            })
+        } else {
+            res.status(401).json({
+                message: "Login failed"
+            })
+        }
+        } catch (error) {
+            res.status(500).json({
+                message: "Failed to log user in"
+            })
+        }
+    })
 
 module.exports = router
