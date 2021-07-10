cc460ba8951106cbac3b1da78b35cc134b2f57f3
Ron Wise <ron@ronwisephoto.com>
2021-07-10 14:23:21 -0400

    tokenization and encryption

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,19 +1,25 @@
 const router = require("express").Router()
 const { UserModel } = require('../models')
 const { UniqueConstraintError } = require("sequelize/lib/errors")
+const jwt = require("jsonwebtoken")
+const bcrypt = require("bcryptjs")
 
 router.post("/register", async (req, res) => {
     let { email, password, name, location } = req.body.user
     try {
         const User = await UserModel.create({
             email,
-            password,
+            password: bcrypt.hashSync(password, 13),
             name,
             location
         })
+
+        let token = jwt.sign({id: User.id,}, process.env.JWT_SECRET, {expiresIn: 60 * 60 * 24})
+
         res.status(201).json({
             message: "User successfully registered",
-            user: User
+            user: User,
+            sessionToken: token
         })
     } catch (err) {
         if (err instanceof UniqueConstraintError) {
@@ -39,10 +45,21 @@ router.post("/login", async (req, res) => {
         })
 
         if (loginUser) {
+
+            let passwordComparison = await bcrypt.compare(password, loginUser.password)
+
+            if (passwordComparison) {
+
+            let token = jwt.sign({id: loginUser.id,}, process.env.JWT_SECRET, {expiresIn: 60 * 60 * 24})
+
             res.status(200).json({
                 user: loginUser,
-                message: "User successfully logged in!"
+                message: "User successfully logged in!",
+                sessionToken: token
             })
+
+        }
+
         } else {
             res.status(401).json({
                 message: "Login failed"
